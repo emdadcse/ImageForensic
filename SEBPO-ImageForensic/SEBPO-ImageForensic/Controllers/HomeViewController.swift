93598acd2b735a536e30f62937a3658c7f899d06
Emdad Hossain <emdad.cse@gmail.com>
2019-09-27 19:56:28 +0600

    First commit

---
+++ SEBPO-ImageForensic/SEBPO-ImageForensic/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  SEBPO-ImageForensic
+//
+//  Created by Emdad Hossain on 27/9/19.
+//  Copyright © 2019 Emdad Hossain. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
+}
+
---
+++ SEBPO-ImageForensic/SEBPO-ImageForensic/Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  SEBPO-ImageForensic
+//
+//  Created by Emdad Hossain on 27/9/19.
+//  Copyright © 2019 Emdad Hossain. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController:UIViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        // todo work here
+    }
+}
