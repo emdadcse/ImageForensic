ab48b1ed0db0b3aca32e785acc6e2ac7357f1d09
Emdad Hossain <emdad.cse@gmail.com>
2019-10-15 23:45:53 +0600

    First commit for FirstBranch

---
+++ SEBPO-ImageForensic/SEBPO-ImageForensic/Controllers/NewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NewModel.swift
+//  SEBPO-ImageForensic
+//
+//  Created by emdad hossain on 15/10/19.
+//  Copyright © 2019 Emdad Hossain. All rights reserved.
+//
+
+import Foundation
+
+class NewModel: NSObject {
+    struct HelloModel {
+        let name: String
+        let age: Int
+    }
+    
+    let helloone = HelloModel(name: "anothe name", age: 23)
+}
